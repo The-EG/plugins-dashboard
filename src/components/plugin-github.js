a6c9d2927eec8741e9a3f4f7bafc7630d131efcf
thelastWallE <12502210+thelastWallE@users.noreply.github.com>
2021-05-05 12:38:22 +0200

    insert link import

--- src/components/plugin-github.js
+++ src/components/plugin-github.js
@@ -5,6 +5,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
+import Link from '@material-ui/core/Link';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
